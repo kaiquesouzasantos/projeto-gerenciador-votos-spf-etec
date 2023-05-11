e63227b776f5f2fad90998a2938eda01885d70d4
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-05-11 12:59:27 -0300

    Add files via upload

--- back-end/src/main/java/gerenciador_notas_spf/controller/RelatorioController.java
+++ back-end/src/main/java/gerenciador_notas_spf/controller/RelatorioController.java
@@ -2,6 +2,7 @@ package gerenciador_notas_spf.controller;
 
 import gerenciador_notas_spf.exception.ExceptionGeneric;
 import gerenciador_notas_spf.model.RelatorioModel;
+import gerenciador_notas_spf.model.SalaModel;
 import gerenciador_notas_spf.service.ApresentacaoService;
 import gerenciador_notas_spf.service.RelatorioService;
 import gerenciador_notas_spf.service.SalaService;
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -23,6 +27,22 @@ public class RelatorioController {
     private final RelatorioService relatorioService;
     private final ApresentacaoService apresentacaoService;
     private final SalaService salaService;
+
+    @GetMapping("/classificacado")
+    public ResponseEntity<List<RelatorioModel>> listAll() {
+        List<RelatorioModel> relatorios = new ArrayList<>();
+
+        try{
+            salaService.listAll().stream().map(SalaModel::getId).forEach(
+                    (sala) -> relatorios.add(this.getRelatorio(sala))
+            );
+        } catch (RuntimeException e){
+            throw new ExceptionGeneric("INFORMACOES INSUFICIENTES", "APRESENTACAO NOT FOUND, INFORMACOES INSUFICIENTES", HttpStatus.NO_CONTENT.value());
+        }
+
+        relatorios.sort(Comparator.comparing(RelatorioModel::getNota));
+        return ResponseEntity.status(HttpStatus.OK).body(relatorios);
+    }
 
     @GetMapping("")
     @Cacheable("relatorio")
